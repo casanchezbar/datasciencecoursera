91f4a0061d4a04a023cf7f01b7e99932a3f49aa5
Carlos Sánchez <casb14@gmail.com>
2019-02-10 19:12:28 -0600

    Assign  function

--- ProgrammingAssignment_GettingAndCleaningData/run_analysis.R
+++ ProgrammingAssignment_GettingAndCleaningData/run_analysis.R
@@ -17,59 +17,61 @@
 #  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 ## =====================================================
 
+run_analysis <- function() {
+  # reads the features column names
+  column_names <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
+  activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
+  
+  # reads and merges X train and test files
+  X_data <- read.table(text = gsub("  ", " ", readLines("UCI HAR Dataset/train/X_train.txt")))
+  X_data <- rbind(X_data, read.table(text = gsub("  ", " ", readLines("UCI HAR Dataset/test/X_test.txt"))))
+  
+  # reads and merges the Y train and test files
+  y_data <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE)
+  y_data <- rbind(y_data, read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE))
+  
+  # reads and merges the subjects train and test files
+  subjects <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
+  subjects <- rbind(subjects, read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE))
+  names(subjects) <- "subject"
+  
+  
+  ## =====================================================
+  #  extracts only the columns related with the mean and standard deviation values
+  ## =====================================================
+  final_column_names <- grep("mean|std", column_names[,2])
+  X_data <- X_data[, final_column_names]
+  
+  
+  ## =====================================================
+  #  Appropriately labels the data set with descriptive variable names.
+  ## =====================================================
+  names(X_data) <- column_names[final_column_names,2]
+  
+  
+  ## =====================================================
+  #  Uses descriptive activity names to name the activities in the data set
+  ## =====================================================
+  y_data[,2] <- apply(y_data, function(x) { activity_names[x,2]})
+  names(y_data) <- c("val", "act_name")
+  
+  
+  
+  # merges the X, y and subjects data
+  final_data <- cbind(y_data$act_name, subjects, X_data )
+  names(final_data)[1] <- "act_name"
+  
+  
+  ## =====================================================
+  #  independent tidy data set with the average of each variable for each activity and each subject
+  ## =====================================================
+  
+  tidy <- aggregate(x = final_data[,c(-1,-2)], by = list(final_data$act_name, final_data$subject), FUN = mean)
+  
+  names(tidy)[1] <- "act_name"
+  names(tidy)[2] <- "subject"
+  
+  tidy
+}
 
-# reads the features column names
-column_names <- read.table("UCI HAR Dataset/features.txt", header=FALSE)
-activity_names <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE)
-
-# reads and merges X train and test files
-X_data <- read.table(text = gsub("  ", " ", readLines("UCI HAR Dataset/train/X_train.txt")))
-X_data <- rbind(X_data, read.table(text = gsub("  ", " ", readLines("UCI HAR Dataset/test/X_test.txt"))))
-
-# reads and merges the Y train and test files
-y_data <- read.table("UCI HAR Dataset/train/y_train.txt", header=FALSE)
-y_data <- rbind(y_data, read.table("UCI HAR Dataset/test/y_test.txt", header=FALSE))
-
-# reads and merges the subjects train and test files
-subjects <- read.table("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
-subjects <- rbind(subjects, read.table("UCI HAR Dataset/test/subject_test.txt", header=FALSE))
-names(subjects) <- "subject"
-
-
-## =====================================================
-#  extracts only the columns related with the mean and standard deviation values
-## =====================================================
-final_column_names <- grep("mean|std", column_names[,2])
-X_data <- X_data[, final_column_names]
-
-
-## =====================================================
-#  Appropriately labels the data set with descriptive variable names.
-## =====================================================
-names(X_data) <- column_names[final_column_names,2]
-
-
-## =====================================================
-#  Uses descriptive activity names to name the activities in the data set
-## =====================================================
-y_data[,2] <- apply(y_data, function(x) { activity_names[x,2]})
-names(y_data) <- c("val", "act_name")
-
-
-
-# merges the X, y and subjects data
-final_data <- cbind(y_data$act_name, subjects, X_data )
-names(final_data)[1] <- "act_name"
-
-
-## =====================================================
-#  independent tidy data set with the average of each variable for each activity and each subject
-## =====================================================
-
-tidy <- aggregate(x = final_data[,c(-1,-2)], by = list(final_data$act_name, final_data$subject), FUN = mean)
-
-names(tidy)[1] <- "act_name"
-names(tidy)[2] <- "subject"
-
-
-write.table(tidy, 'assigment_data_results.txt', row.names = FALSE) 
+write.table(run_analysis(), 'assigment_data_results.txt', row.names = FALSE) 
